60c325f2acf71baa8fb17d8113c0a3cd00727aaa
Khalid Al-Yafey <khaled.q@neumtech.com>
2025-07-28 13:09:15 +0300

    update cors origin again

--- src/main/java/com/nakqeeb/ecommerce/config/SecurityConfiguration.java
+++ src/main/java/com/nakqeeb/ecommerce/config/SecurityConfiguration.java
@@ -61,7 +61,8 @@ public class SecurityConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        // configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
